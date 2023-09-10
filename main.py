cf0d774d0ab1f254ac52cd1ce4309543b0ff3f04
Navyam-Raushan <125335352+Navyam-Raushan@users.noreply.github.com>
2023-09-10 18:47:09 +0530

    Raising some error but working.

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import random
 from pprint import pprint
 from send_mail import send_gmail
 
+topic = "amazon"
 API_KEY = 'da0e6e21135e4b11b4eec62705093df8'
-url = "https://newsapi.org/v2/everything?q=apple&" \
-      "from=2023-09-09&to=2023-09-09&sortBy=popularity&" \
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "from=2023-09-09&to=2023-09-09&" \
+      "sortBy=popularity&" \
       "apiKey=da0e6e21135e4b11b4eec62705093df8"
 
 request = requests.get(url=url)
@@ -13,33 +16,13 @@ request = requests.get(url=url)
 content = request.json()
 
 # ACCESSING DIFFERENT PART OF ARTICLE.
-title = []
-author = []
-description = []
-
-for article in content["articles"]:
+body = ""
+for article in content["articles"][:10]:
     if article["title"] is not None:
-        title.append(article["title"])
-    if article["author"] is not None:
-        author.append(article["author"])
-    description.append(article["description"])
+        body = "Subject: Today's News" \
+               + "\n" + body + article["title"] + "\n" \
+               + article["description"] \
+               + "\n" + article["url"] + 2 * "\n"
 
-chosen_title = random.choice(title)
-
-index = title.index(chosen_title)
-chosen_author = author[index]
-chosen_des = description[index]
-
-# the encoding is causing problem only.
-chosen_des = chosen_des.encode("utf-8")
-
-message = f"""\
-Subject: {chosen_title}
-
-From: {chosen_author}
- 
-{chosen_des}
-"""
-
-send_gmail(message)
-print(message)
+body = body.encode("utf-8")
+send_gmail(message=body)
